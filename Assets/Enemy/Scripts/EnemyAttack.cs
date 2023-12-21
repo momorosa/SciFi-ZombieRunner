14ea4e0eecc85f7b4765ad629f8bd58eef7c60a9
Rosa Choi <122864464+momorosa@users.noreply.github.com>
2023-12-21 13:27:13 -0800

    Player Health
    Create Player Health Class so that enemy can hit and damage the player

--- Assets/Enemy/Scripts/EnemyAttack.cs
+++ Assets/Enemy/Scripts/EnemyAttack.cs
@@ -4,17 +4,18 @@ using UnityEngine;
 
 public class EnemyAttack : MonoBehaviour
 {
-    [SerializeField] Transform target;
     [SerializeField] float damage = 40f;
+
+    PlayerHealth target;
 
     void Start()
     {
-        
+        target = FindObjectOfType<PlayerHealth>();
     }
 
     public void AttackHitEvent()
     {
         if (target == null) return;
-        Debug.Log("bang, bang");
+        target.TakeDamage(damage);
     }
 }
