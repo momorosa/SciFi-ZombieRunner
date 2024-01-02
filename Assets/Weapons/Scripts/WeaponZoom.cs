85c078c78b076c652a836e37469654a388f27823
Rosa Choi <122864464+momorosa@users.noreply.github.com>
2024-01-02 15:53:48 -0800

    Weapon Zoom
    Use a main camera's filed of view to zoom in and out

---
+++ Assets/Weapons/Scripts/WeaponZoom.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponZoom : MonoBehaviour
+{
+    [SerializeField] Camera fpsCamera;
+    [SerializeField] float zoomedInFOV = 20f;
+    [SerializeField] float zoomedOutFOV = 60f;
+
+    bool zoomedInToggle = false;
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(1))
+        {
+            if (zoomedInToggle == false)
+            {
+                zoomedInToggle = true;
+                fpsCamera.fieldOfView = zoomedInFOV;
+            }
+            else
+            {
+                zoomedInToggle = false;
+                fpsCamera.fieldOfView = zoomedOutFOV;
+            }
+        }
+    }
+
+
+}
