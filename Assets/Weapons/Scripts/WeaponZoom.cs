3a7d6f7d6e1008a660ea294be13de057076610ba
Rosa Choi <122864464+momorosa@users.noreply.github.com>
2024-01-02 22:47:19 -0800

    Weapon Zoom Rotation Sensitivity
    Update Rotation speed/sensitivity based upon zoom in and out

--- Assets/Weapons/Scripts/WeaponZoom.cs
+++ Assets/Weapons/Scripts/WeaponZoom.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using StarterAssets;
 using UnityEngine;
 
 public class WeaponZoom : MonoBehaviour
@@ -7,8 +8,18 @@ public class WeaponZoom : MonoBehaviour
     [SerializeField] Camera fpsCamera;
     [SerializeField] float zoomedInFOV = 20f;
     [SerializeField] float zoomedOutFOV = 60f;
+    [SerializeField] float zoomInSensitivity = 1f;
+    [SerializeField] float zoomOutSensitivity = 2f;
+
+    FirstPersonController fpsController;
 
     bool zoomedInToggle = false;
+
+
+    void Start()
+    {
+        fpsController = GetComponent<FirstPersonController>();
+    }
 
     void Update()
     {
@@ -18,11 +29,13 @@ public class WeaponZoom : MonoBehaviour
             {
                 zoomedInToggle = true;
                 fpsCamera.fieldOfView = zoomedInFOV;
+                fpsController.RotationSpeed = zoomInSensitivity;
             }
             else
             {
                 zoomedInToggle = false;
                 fpsCamera.fieldOfView = zoomedOutFOV;
+                fpsController.RotationSpeed = zoomOutSensitivity;
             }
         }
     }
